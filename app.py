72b6a2933a511f967f143dc4227d784ec656af3f
Zhilenkoff <M.Zhilenkoff@yandex.ru>
2022-09-28 19:47:13 +0300

    добавил комментарий

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 #добавил комментарий
 @app.route('/')
-@app.route('/index/')
-def hello():
+@app.route('/tex/')
+def text():
     return render_template('12.html')
 
+@app.route('/hello/')
+def hello():
+    return render_template('index.html')
 
-
+#динамичная страница
+@app.route('/<name>')
+def users(name):
+    return render_template('nick.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
